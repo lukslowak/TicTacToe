cdf451141164754fcf7901f697151944fc7a81f4
lukslowak <34584607+lukslowak@users.noreply.github.com>
2018-08-24 21:54:00 +0200

    Update test.java

--- src/sample/test.java
+++ src/sample/test.java
@@ -5,6 +5,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("dodajemy kolejną zmianę");
         System.out.println("zmiana2");
+        System.out.println("zmiana3");
     }
 
 }
